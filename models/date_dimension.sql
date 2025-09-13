dd6994bd4dd06bcce57649777f9e5b64de4af849
Yevhenii Zenchenko <ybzenchenko@gmail.com>
2025-09-13 11:48:31 +0000

    data correction commit

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -7,7 +7,7 @@ select
 {{day_type('STARTED_AT')}} AS DAY_TYPE,
 {{get_season('STARTED_AT')}} AS STATION_OF_YEAR
 
-from {{ source('demo', 'bike') }}
+from {{ ref('stg_bike') }}
 where STARTED_AT != 'started_at'
 )
 select *
